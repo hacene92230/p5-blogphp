95978e83d7d3f8ff9b5a1f88be8d5b45f344b86b
--user.email <hacenedu92@live.fr>
2022-12-08 22:58:29 +0100

    amélioration du socle de travail

--- App/Views/layout/footer.php
+++ App/Views/layout/footer.php
@@ -1,6 +1,6 @@
    <footer>
     <ul>
-            <li> <a href='index.php?page=accueil&action=cgc'>Conditions générales des concours</a></li>
+            <li> <a href='index.php?page=accueil&action=cgc'>Conditions générales.</a></li>
             <li> <a href='index.php?page=accueil&action=cgu'>Conditions générales d'utilisation</a></li>
             <li> <a href='index.php?page=accueil&action=contact'>Nous contacter</a></li>
 </ul>
