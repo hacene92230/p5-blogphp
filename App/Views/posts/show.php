6d3368dacc6e8b0d9bc4be662cb6e65603c5870a
--user.email <hacenedu92@live.fr>
2022-12-16 16:59:42 +0100

    bug fix when displaying a comment.

--- App/Views/posts/show.php
+++ App/Views/posts/show.php
@@ -5,7 +5,7 @@ echo "<p>" . $showtab['content'] . "</p>";
 <h2>Commentaires</h2>
 <?php
 foreach ($show_comments as $cle => $valeur) {
-    echo "<p>".$valeur["content"]."</p>";
+    print("<p>" . $valeur["content"] . "</p>");
 }
 if (isset($_SESSION["utilisateurs"])) {
     include("../App/Views/comments/new.php");
