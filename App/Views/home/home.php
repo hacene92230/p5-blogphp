b54c7e95a5e65dab1a8b406d49df376155c51dab
--user.email <hacenedu92@live.fr>
2022-12-21 16:19:39 +0100

    final of project

--- App/Views/home/home.php
+++ App/Views/home/home.php
@@ -1,5 +1,5 @@
-<?php
-foreach ($tabpost as $cle => $valeur) {
-    echo "<h2>" . $valeur->title . "</h2>";
-    echo "<a href='index.php?page=posts&action=show&post=$valeur->id'>Consulter</a>";
-}
+ <?php
+    foreach ($tabpost as $cle => $valeur) {
+        echo "<h2>" . $valeur->title . "</h2>";
+        echo "<a href='index.php?page=posts&action=show&post=$valeur->id'>Consulter</a>";
+    }
