34372dd72bfd143db851efee64544392edc95cea
--user.email <hacenedu92@live.fr>
2023-01-05 18:02:06 +0100

    correction of different psr in the comments model file

--- App/Models/Comments.php
+++ App/Models/Comments.php
@@ -9,21 +9,37 @@ class Comments extends Model
 {
     protected static $table = 'comments';
     protected static $primary = 'id';
-    //Deletes the comments of a user passed in parameter.
-    public function delete_comment_of_user(int $user)
+
+    /**
+     * Supprime les commentaires d'un utilisateur passé en paramètre
+     *
+     * @param int $user ID de l'utilisateur
+     * @return array
+     */
+    public function delete_comments_of_user(int $user): array
     {
-        return $this->request('delete FROM comments WHERE user_id = :user', [':user' => $user], PDO::FETCH_ASSOC);
+        return $this->request('DELETE FROM comments WHERE user_id = :user', [':user' => $user], PDO::FETCH_ASSOC);
     }
 
-    //Retrieves the first name of a user who submitted a comment.
-    public function get_user_of_comment($user_id)
+    /**
+     * Récupère le prénom d'un utilisateur ayant soumis un commentaire
+     *
+     * @param int $user_id ID de l'utilisateur
+     * @return array
+     */
+    public function get_user_of_comment(int $user_id): array
     {
-        return $this->request('SELECT firstname FROM users, comments WHERE comments.user_id = :user_id', [':user_id' => $user_id], PDO::FETCH_ASSOC);
+        return $this->request('SELECT firstname FROM users u INNER JOIN comments c ON c.user_id = u.id WHERE c.user_id = :user_id', [':user_id' => $user_id], PDO::FETCH_ASSOC);
     }
 
-    //Retrieves approved comments belonging to an article.
-    public function get_comment_approve_by_post($post_id)
+    /**
+     * Récupère les commentaires approuvés appartenant à un article
+     *
+     * @param int $post_id ID de l'article
+     * @return array
+     */
+    public function get_comments_approved_by_post(int $post_id): array
     {
-        return $this->request('SELECT *FROM comments WHERE approval = 1 and posts_id = :post_id', [':post_id' => $post_id], PDO::FETCH_ASSOC);
+        return $this->request('SELECT * FROM comments WHERE approval = 1 AND posts_id = :post_id', [':post_id' => $post_id], PDO::FETCH_ASSOC);
     }
 }
--- System/Base/DB.php
+++ System/Base/DB.php
@@ -1,39 +1,50 @@
 <?php
-namespace System\Base {
-    use PDO;
-	use PDOException;
+
+namespace System\Base;
+
+use PDO;
+use PDOException;
+
+/**
+ * Class utilisée pour obtenir une instance de PDO
+ * Utilisation :
+ * $pdo = DB::get();
+ */
+class DB
+{
+    // Constantes définissant les informations de connexion à la base de données
+    const DATABASE = 'p5';
+    const USERNAME = 'root';
+    const PASSWORD = '';
+
+    // Variable statique stockant l'instance de PDO
+    private static $pdo;
+
     /**
-     * Class utiliser pour avoir une instance de PDO
-     * utilisation : 
-     * $pdo = DB::get();
+     * Méthode statique retournant l'instance de PDO
+     * Si l'instance n'a pas encore été créée, elle est initialisée
+     *
+     * @return PDO
+     * @throws PDOException Si une erreur de connexion se produit
      */
-    class DB    
+    public static function get(): PDO
     {
-        const DATABASE = 'p5';
-        const USERNAME = 'root';
-        const PASSWORD = '';
-        private static $pdo;
-        public static function get(): PDO
-        {
-            if (is_null(static::$pdo))
-	    	{
-        		try
-        		{
-            		static::$pdo = new PDO(
-                    	sprintf(
-                        	'mysql:host=localhost;port=3306;dbname=%s;charset=UTF8',
-                        	self::DATABASE),
-                    		self::USERNAME,
-                    		self::PASSWORD);
-	static::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-              		return static::$pdo;
-  				}
-				catch( PDOException $Exception)
-      			{
-            		die ('Erreur de connection :'.$errorconnexion->getMessage());
-      			}
-	    	}
-	    	return static::$pdo;
-		}
+        if (is_null(self::$pdo)) {
+            try {
+                self::$pdo = new PDO(
+                    sprintf(
+                        'mysql:host=localhost;port=3306;dbname=%s;charset=UTF8',
+                        self::DATABASE
+                    ),
+                    self::USERNAME,
+                    self::PASSWORD
+                );
+                self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                return self::$pdo;
+            } catch (PDOException $exception) {
+                die('Erreur de connection :' . $exception->getMessage());
+            }
+        }
+        return self::$pdo;
     }
 }
