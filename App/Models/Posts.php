0114b9a7dc30c50deef247517c50b31e8b7487de
--user.email <hacenedu92@live.fr>
2023-01-05 18:19:07 +0100

    I did the same thing for the articles, I put comments, and improved the maintainability of the code, respecting certain psr, I modified the naming of variables.

--- App/Models/Posts.php
+++ App/Models/Posts.php
@@ -5,13 +5,56 @@ namespace App\Models;
 use System\Coeur\Models\Model;
 use PDO;
 
-class Posts extends Model
+class Comments extends Model
 {
-    protected static $table = 'posts';
+    protected static $table = 'comments';
     protected static $primary = 'id';
-    //Retrieves an article with its identifier passed as a parameter. 
-    public function get_id_post(int $id)
+
+    /**
+     * Supprime les commentaires d'un utilisateur passé en paramètre
+     *
+     * @param int $user ID de l'utilisateur
+     * @return bool
+     * @throws PDOException Si la requête SQL échoue
+     */
+    public function delete_comments_by_user(int $user): bool
     {
-        return $this->request('SELECT * FROM posts WHERE id = :id', [':id' => $id], PDO::FETCH_ASSOC);
+        try {
+            return $this->request('DELETE FROM comments WHERE user_id = :user', [':user' => $user], PDO::FETCH_ASSOC);
+        } catch (PDOException $e) {
+            throw $e;
+        }
+    }
+
+    /**
+     * Récupère le prénom d'un utilisateur qui a soumis un commentaire
+     *
+     * @param int $user_id ID de l'utilisateur
+     * @return array
+     * @throws PDOException Si la requête SQL échoue
+     */
+    public function get_firstname_by_comment_user(int $user_id): array
+    {
+        try {
+            return $this->request('SELECT firstname FROM users, comments WHERE comments.user_id = :user_id', [':user_id' => $user_id], PDO::FETCH_ASSOC);
+        } catch (PDOException $e) {
+            throw $e;
+        }
+    }
+
+    /**
+     * Récupère les commentaires approuvés appartenant à un article
+     *
+     * @param int $post_id ID de l'article
+     * @return array
+     * @throws PDOException Si la requête SQL échoue
+     */
+    public function get_approved_comments_by_post(int $post_id): array
+    {
+        try {
+            return $this->request('SELECT * FROM comments WHERE approval = 1 AND posts_id = :post_id', [':post_id' => $post_id], PDO::FETCH_ASSOC);
+        } catch (PDOException $e) {
+            throw $e;
+        }
     }
 }
